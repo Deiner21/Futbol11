8d3b5f072a3d47f278acfdf9bede1edd7053aa50
Deiner21 <89552349+Deiner21@users.noreply.github.com>
2024-08-10 13:53:24 -0500

    Add files via upload

---
+++ inicio.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Descargar Futbol 11</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Descarga Futbol 11</h1>
+        <p>Haz clic en el botón de abajo para descargar la APP.</p>
+        <a class="download-btn" href="https://www.mediafire.com/file/x3j45shgrd626cf/Futbol+11.apk/file">Descargar</a>
+    </div>
+</body>
+</html>
